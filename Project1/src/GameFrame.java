a91bbeeb48f6ef72523781bfd6e3774c6957fb4e
Farhad Nezamy <f.nezamy@gmail.com>
2017-11-07 12:37:53 -0500

    Update GameFrame.java

--- Project1/src/GameFrame.java
+++ Project1/src/GameFrame.java
@@ -10,31 +10,31 @@ import javax.swing.JFrame;
 public class GameFrame {
 	public GameFrame(){
 		final int WIDTH = 800;//height of the game's frame
-    	final int HEIGHT = WIDTH / 16 * 9;//width of the games frame
-    	final Dimension gameSize = new Dimension(WIDTH, HEIGHT);//set the dimensions of the game
-        JFrame frame = new JFrame("Pong");
-        frame.setMinimumSize(gameSize);//Set the minimum size of the game
+    		final int HEIGHT = WIDTH / 16 * 9;//width of the games frame
+    		final Dimension gameSize = new Dimension(WIDTH, HEIGHT);//set the dimensions of the game
+        	JFrame frame = new JFrame("Pong");
+        	frame.setMinimumSize(gameSize);//Set the minimum size of the game
 		frame.setPreferredSize(gameSize);//set the preferred size of the game
 		frame.setMaximumSize(gameSize);//set the maximum size of the game
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//close the game on clicking the X
-        frame.setResizable(false);//can not resize the game
-        frame.setLocationRelativeTo(null);//center the game frame
-        frame.setVisible(true);//set it visable
-        frame.setLayout(new BorderLayout());
-        InputHandler ih = new InputHandler();//declare and initialize the input handler
+        	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//close the game on clicking the X
+        	frame.setResizable(false);//can not resize the game
+        	frame.setLocationRelativeTo(null);//center the game frame
+        	frame.setVisible(true);//set it visable
+        	frame.setLayout(new BorderLayout());
+        	InputHandler ih = new InputHandler();//declare and initialize the input handler
         
-        // bare bones: just add a panel where the game objects are drawn
+        	// bare bones: just add a panel where the game objects are drawn
         
-        GamePanel gamePanel = new GamePanel();
-        frame.add(gamePanel, BorderLayout.CENTER);
-        frame.pack();//pack everything in
+        	GamePanel gamePanel = new GamePanel();
+        	frame.add(gamePanel, BorderLayout.CENTER);
+        	frame.pack();//pack everything in
         
-        frame.setSize(WIDTH,HEIGHT);
-        frame.addKeyListener(ih);//add the key listener
+        	frame.setSize(WIDTH,HEIGHT);
+        	frame.addKeyListener(ih);//add the key listener
       
-        // the game starts when the game panel animation begins
+        	// the game starts when the game panel animation begins
         
-        gamePanel.go();
+        	gamePanel.go();
 	}
 
 }
